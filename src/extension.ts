1584823d1637f74f7e01f64e8abb4a99b41287b1
fengtan <Fengtan@847318.no-reply.drupal.org>
2022-08-20 22:28:33 +0000

    Activate extension when view is shown.

--- src/extension.ts
+++ src/extension.ts
@@ -2,15 +2,15 @@ import * as vscode from 'vscode';
 import { LdapProvider } from './provider';
 
 // This method is called when the extension is activated.
+// The extension is activated when the view is shown (see activationEvents in package.json).
 export function activate(context: vscode.ExtensionContext) {
 	
 	// @todo drop
 	console.log('test activate');
 
-	// @todo uncomment
-	/*vscode.window.createTreeView('ldap-browser-view', {
+	vscode.window.createTreeView('ldap-browser-view', {
 		treeDataProvider: new LdapProvider()
-	});*/
+	});
 	
 }
 
