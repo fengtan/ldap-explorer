a6ed31532635c33000959e742476a023148d14eb
fengtan <Fengtan@847318.no-reply.drupal.org>
2022-08-25 01:41:12 +0000

    Refactor.

--- src/ldapDataProvider.ts
+++ src/ldapDataProvider.ts
@@ -3,11 +3,11 @@
 import * as vscode from 'vscode';
 import { LdapConnectionManager } from './ldapConnectionManager';
 import { LdapTreeItem } from './ldapTreeItem';
-import * as ldapjs from 'ldapjs'; // @todo may not need to import *
 
 export class LdapDataProvider implements vscode.TreeDataProvider<LdapTreeItem> {
 
   getTreeItem(treeItem: LdapTreeItem): vscode.TreeItem {
+    // @todo move this logic into LdapTreeItem and just return treeItem (have LdapTreeItem extend TreeItem)
     const collapsiblestate = treeItem.isExpandable() ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None;
     let item = new vscode.TreeItem(treeItem.getLabel(), collapsiblestate);
     item.description = treeItem.getDescription();
@@ -17,73 +17,14 @@ export class LdapDataProvider implements vscode.TreeDataProvider<LdapTreeItem> {
 
   getChildren(treeItem?: LdapTreeItem): Thenable<LdapTreeItem[]> {
     // No element passed i.e. we are at the root of the tree.
-    // Build list of connections from settings.
+    // Extract list of connections from settings.
     if (!treeItem) {
-      return Promise.resolve(LdapConnectionManager.getConnections().map(connection => new LdapTreeItem(connection)));
+      const connections = LdapConnectionManager.getConnections();
+      return Promise.resolve(connections.map(connection => new LdapTreeItem(connection)));
     }
 
-    // A valid element was passed i.e. we need to list the LDAP children (CN, OU, etc) of this parent.
-    const connection = treeItem.getLdapConnection();
-    return new Promise((resolve, reject) => {
-      // Create LDAP object.
-      const client = ldapjs.createClient({
-        url: [connection.getUrl()]
-      });
-
-      // Bind.
-      client.bind(connection.binddn, connection.bindpwd, (err) => {
-        if (err) {
-          // @todo same comments as client.on below.
-          console.log(err); // @todo drop ?
-          vscode.window.showErrorMessage(`Error when binding: ${err}`);
-          client.destroy(); // @todo should destroy client at any other place where we handle an error
-          return resolve([]); // @todo should reject() instead of resolve([])
-        }
-
-        // Search.
-        // @todo set additional options ? Second argument of the search() method
-        // @todo clean this messy search() call - should call reject() or resolve() etc
-        // Set LDAP search scope of "one" so we get only immediate subordinates of the base DN https://ldapwiki.com/wiki/SingleLevel
-        client.search(treeItem.getDN(), {"scope": "one"}, (err, res) => {
-          console.log(err); // @todo handle and return if there is an error
-  
-          let results: LdapTreeItem[] = [];
-          res.on('searchRequest', (searchRequest) => {
-            console.log('searchRequest: ', searchRequest.messageID);
-          });
-          res.on('searchEntry', (entry) => {
-            results.push(new LdapTreeItem(connection, entry.dn)); // @todo best to show only the OU/CN name instead of the full DN ? For UX
-            console.log('entry: ' + JSON.stringify(entry.object));
-          });
-          res.on('searchReference', (referral) => {
-            console.log('referral: ' + referral.uris.join());
-          });
-          res.on('error', (err) => {
-            console.error('error: ' + err.message);
-          });
-          res.on('end', (result) => {
-            // @todo verify status is 0 ?
-            console.log('status: ' + result!.status);
-            return resolve(results);
-          });
-        });
-
-        // @todo should unbind somewhere (maybe in a callback) see https://github.com/ldapjs/node-ldapjs/issues/428
-      });
-    });
-
-
-    /*
-    @todo uncomment ?
-    client.on('error', (err) => {
-      // @todo wording (find something better than just "Error: XX")
-      // @todo handle different types of error ? http://ldapjs.org/errors.html
-      // @todo test (when host is invalid, when bind dn does not work, when password does not work, etc)
-      console.log(err);
-      vscode.window.showErrorMessage(`Error (regular): ${err}`);
-      return Promise.resolve([]);
-    });
-    */    
+    // Otherwise get chilren of the TreeItem.
+    return treeItem.getChildren();
   }
 
   // Logic to refresh the view.
--- src/ldapTreeItem.ts
+++ src/ldapTreeItem.ts
@@ -1,6 +1,8 @@
 // Represents an item in the tree view (either a connection or an lDAP result).
 
 import { LdapConnection } from "./ldapConnection";
+import * as ldapjs from 'ldapjs'; // @todo may not need to import *
+import * as vscode from 'vscode';
 
 export class LdapTreeItem {
 
@@ -51,11 +53,73 @@ export class LdapTreeItem {
     return this.connection;
   }
 
-  // DN representing the TreeItem
-  // - If the TreeItem is an LDAP result, then this is its regular DN.
-  // - If the TreeItem is a connection, then the DN its the base DB configured by the user.
-  getDN(): string {
-    return this.dn ?? this.connection.basedn;
+  // Get children of this tree item.
+  getChildren(): Thenable<LdapTreeItem[]> {
+    return new Promise((resolve, reject) => {
+
+      // Create LDAP object.
+      const client = ldapjs.createClient({
+        url: [this.connection.getUrl()]
+      });
+
+      // Bind.
+      client.bind(this.connection.binddn, this.connection.bindpwd, (err) => {
+        if (err) {
+          // @todo same comments as client.on below.
+          console.log(err); // @todo drop ?
+          vscode.window.showErrorMessage(`Error when binding: ${err}`); // @todo no, should throw exception and handle error in LdapDataProvider.ts, this class should only be about ldapjs, not about VS Code UI
+          client.destroy(); // @todo should destroy client at any other place where we handle an error
+          return resolve([]); // @todo should reject() instead of resolve([])
+        }
+
+        // Prepare the DN we will query.
+        // - If the TreeItem is an LDAP result, then we will query its regular DN.
+        // - If the TreeItem is a connection, then we will query the base DN configured by the user.
+        const dn = this.dn ?? this.connection.basedn;
+        // Search.
+        // @todo set additional options ? Second argument of the search() method
+        // @todo clean this messy search() call - should call reject() or resolve() etc
+        // Set LDAP search scope of "one" so we get only immediate subordinates of the base DN https://ldapwiki.com/wiki/SingleLevel
+        client.search(dn, {"scope": "one"}, (err, res) => {
+          console.log(err); // @todo handle and return if there is an error
+  
+          let results: LdapTreeItem[] = [];
+          res.on('searchRequest', (searchRequest) => {
+            console.log('searchRequest: ', searchRequest.messageID);
+          });
+          res.on('searchEntry', (entry) => {
+            results.push(new LdapTreeItem(this.connection, entry.dn)); // @todo best to show only the OU/CN name instead of the full DN ? For UX
+            console.log('entry: ' + JSON.stringify(entry.object));
+          });
+          res.on('searchReference', (referral) => {
+            console.log('referral: ' + referral.uris.join());
+          });
+          res.on('error', (err) => {
+            console.error('error: ' + err.message);
+          });
+          res.on('end', (result) => {
+            // @todo verify status is 0 ?
+            console.log('status: ' + result!.status);
+            return resolve(results);
+          });
+        });
+
+        // @todo should unbind somewhere (maybe in a callback) see https://github.com/ldapjs/node-ldapjs/issues/428
+      });
+    });
+
+
+    /*
+    @todo uncomment ?
+    client.on('error', (err) => {
+      // @todo wording (find something better than just "Error: XX")
+      // @todo handle different types of error ? http://ldapjs.org/errors.html
+      // @todo test (when host is invalid, when bind dn does not work, when password does not work, etc)
+      console.log(err);
+      vscode.window.showErrorMessage(`Error (regular): ${err}`);
+      return Promise.resolve([]);
+    });
+    */    
   }
 
 }
