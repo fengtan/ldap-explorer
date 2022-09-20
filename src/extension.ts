d505ce89f74e46b6342f8f190eb8a3fdfcf5bef4
fengtan <Fengtan@847318.no-reply.drupal.org>
2022-09-19 21:23:44 -0400

    Refactor.

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,8 @@
 import * as vscode from 'vscode';
 import { LdapConnectionManager } from './ldapConnectionManager';
-import { LdapConnection } from './ldapConnection';
 import { LdapDataProvider } from './ldapDataProvider';
 import { LdapTreeItem } from './ldapTreeItem';
-import { getWebviewUiToolkitUri } from './utilities';
-import { showAttributesInWebview } from './webviews';
+import { createAttributesWebview, createAddConnectionWebview } from './webviews';
 
 // This method is called when the extension is activated (see activationEvents in package.json).
 export function activate(context: vscode.ExtensionContext) {
@@ -15,60 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Implement "Add connection" command (declared in package.json).
 	context.subscriptions.push(vscode.commands.registerCommand('ldap-explorer.add-connection', () => {
-		// Create webview.
-		const panel = vscode.window.createWebviewPanel(
-			'ldap-explorer.add-connection',
-			'LDAP Explorer: Add new connection',
-			vscode.ViewColumn.One,
-			{
-				enableScripts: true
-			}
-		);
-
-		// Populate webview content.
-		panel.webview.html = getAddNewConnectionHTML(panel.webview, context.extensionUri);
-
-		// Handle messages from webview to the extension.
-		// See https://code.visualstudio.com/api/extension-guides/webview#passing-messages-from-a-webview-to-an-extension
-		panel.webview.onDidReceiveMessage(
-			message => {
-				// Build connection object.
-				const connection = new LdapConnection(
-					message.name,
-					message.protocol,
-					message.host,
-					message.port,
-					message.binddn,
-					message.bindpwd,
-					message.basedn
-				);
-				switch (message.command) {
-					case 'save':
-						// Save connection to settings.
-						LdapConnectionManager.addConnection(connection);
-
-						// Refresh view so the new connection shows up.
-						// @todo focus on tree view (in case it was not open) ?
-				  		vscode.commands.executeCommand("ldap-explorer.refresh-view");
-
-						// @todo UX: message "connection was added to your settings" (along with JSON object and location of the settings file i.e. whether it was stored in global or workspace settings)
-				  		return;
-
-					case 'test':
-						// Test connection.
-						connection.search({}).then(
-							value => {
-								vscode.window.showInformationMessage('Connection succeeded');
-							},
-							reason => {
-								vscode.window.showErrorMessage('Connection failed: ' + reason);
-							}
-						);
-			  	}
-			},
-			undefined,
-			context.subscriptions
-		  );
+		createAddConnectionWebview(context);
 	}));
 
 	// Implement "Delete connection" command.
@@ -98,7 +43,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Implement "Show attributes" command (show attributes of the DN in a webview).
 	context.subscriptions.push(vscode.commands.registerCommand('ldap-explorer.show-attributes', (treeItem: LdapTreeItem) => {
-		showAttributesInWebview(treeItem.getLdapConnection(), treeItem.getDn(), context.extensionUri);
+		createAttributesWebview(treeItem.getLdapConnection(), treeItem.getDn(), context);
 	}));
 
 	// @todo is it necessary to pass all registered commands through context.subscriptions.push() ?
@@ -111,68 +56,4 @@ export function deactivate() {
 	// @todo clear provider ? See todo-tree
 	// @todo unbind all connections http://ldapjs.org/client.html#unbind
 
-}
-
-// @todo drop these arguments, this makes no sense
-function getAddNewConnectionHTML(webview: vscode.Webview, extensionUri: vscode.Uri) {
-	// @todo ideally generate the form by inspecting package.json configuration contribution
-
-	// We need to include this JS into the webview in order to use the Webview UI toolkit.
-	const toolkitUri = getWebviewUiToolkitUri(webview, extensionUri);
-
-	return `<!DOCTYPE html>
-	<html lang="en">
-		<head>
-			<script type="module" src="${toolkitUri}"></script>
-  		</head>
-		<body>
-			<section>
-				<vscode-text-field type="text" id="name" placeholder="e.g. My connection" autofocus>Connection name</vscode-text-field>
-			</section>
-			<section>
-				<p>Protocol</p>
-				<vscode-dropdown id="protocol">
-					<vscode-option>ldap</vscode-option>
-					<vscode-option>ldaps</vscode-option>
-				</vscode-dropdown>
-			</section>
-			<section>
-				<vscode-text-field type="text" id="host" placeholder="e.g. example.net">Host</vscode-text-field>
-			</section>
-			<section>
-				<vscode-text-field type="text" id="port" placeholder="e.g. 389 or 636">Port</vscode-text-field>
-			</section>
-			<section>
-				<vscode-text-field type="text" id="binddn" placeholder="e.g. cn=admin,dc=example,dc=org">Bind DN</vscode-text-field>
-			</section>
-			<section>
-				<vscode-text-field type="text" id="bindpwd">Bind Password</vscode-text-field>
-			</section>
-			<section>
-				<vscode-text-field type="text" id="basedn" placeholder="e.g. dc=example,dc=org">Base DN</vscode-text-field>
-			</section>
-
-			<!-- TODO add spacing between form elements -->
-			<!-- TODO complain if the connection name submitted already exists (must be unique) -->
-			<!-- TODO some form elements should be mandatory -->
-
-			<vscode-button onClick="submitForm('save')">Save</vscode-button>
-			<vscode-button onClick="submitForm('test')" appearance="secondary">Test</vscode-button>
-			<script>
-				const vscode = acquireVsCodeApi();
-				function submitForm(command) {
-					vscode.postMessage({
-						command: command,
-						name: document.getElementById("name").value,
-						protocol: document.getElementById("protocol").value,
-						host: document.getElementById("host").value,
-						port: document.getElementById("port").value,
-						binddn: document.getElementById("binddn").value,
-						bindpwd: document.getElementById("bindpwd").value,
-						basedn: document.getElementById("basedn").value
-					});
-				}
-			</script>
-		</body>
-	</html>`;
 }
--- src/webviews.ts
+++ src/webviews.ts
@@ -1,8 +1,9 @@
 import { LdapConnection } from './ldapConnection';
+import { LdapConnectionManager } from './ldapConnectionManager';
 import { getWebviewUiToolkitUri } from "./utilities";
 import * as vscode from 'vscode';
 
-export function showAttributesInWebview(ldapConnection: LdapConnection, dn: string, extensionUri: vscode.Uri) {
+export function createAttributesWebview(ldapConnection: LdapConnection, dn: string, context: vscode.ExtensionContext) {
 	// Create webview.
 	const panel = vscode.window.createWebviewPanel(
 		'ldap-explorer.show-attributes',
@@ -18,7 +19,7 @@ export function showAttributesInWebview(ldapConnection: LdapConnection, dn: stri
 	ldapConnection.search({scope: "base"}, dn).then(entries => {
 	
 		// We need to include this JS into the webview in order to use the Webview UI toolkit.
-		const toolkitUri = getWebviewUiToolkitUri(panel.webview, extensionUri);
+		const toolkitUri = getWebviewUiToolkitUri(panel.webview, context.extensionUri);
 	
 		panel.webview.html =
 		  `<!DOCTYPE html>
@@ -73,4 +74,122 @@ export function showAttributesInWebview(ldapConnection: LdapConnection, dn: stri
 		});
 	
 	});
+}
+
+export function createAddConnectionWebview(context: vscode.ExtensionContext) {
+
+	// @todo ideally generate the form by inspecting package.json configuration contribution
+
+    // Create webview.
+    const panel = vscode.window.createWebviewPanel(
+        'ldap-explorer.add-connection',
+        'LDAP Explorer: Add new connection',
+        vscode.ViewColumn.One,
+        {
+            enableScripts: true
+        }
+    );
+
+    // We need to include this JS into the webview in order to use the Webview UI toolkit.
+	const toolkitUri = getWebviewUiToolkitUri(panel.webview, context.extensionUri);
+
+    // Populate webview content.
+    panel.webview.html = `<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<script type="module" src="${toolkitUri}"></script>
+  		</head>
+		<body>
+			<section>
+				<vscode-text-field type="text" id="name" placeholder="e.g. My connection" autofocus>Connection name</vscode-text-field>
+			</section>
+			<section>
+				<p>Protocol</p>
+				<vscode-dropdown id="protocol">
+					<vscode-option>ldap</vscode-option>
+					<vscode-option>ldaps</vscode-option>
+				</vscode-dropdown>
+			</section>
+			<section>
+				<vscode-text-field type="text" id="host" placeholder="e.g. example.net">Host</vscode-text-field>
+			</section>
+			<section>
+				<vscode-text-field type="text" id="port" placeholder="e.g. 389 or 636">Port</vscode-text-field>
+			</section>
+			<section>
+				<vscode-text-field type="text" id="binddn" placeholder="e.g. cn=admin,dc=example,dc=org">Bind DN</vscode-text-field>
+			</section>
+			<section>
+				<vscode-text-field type="text" id="bindpwd">Bind Password</vscode-text-field>
+			</section>
+			<section>
+				<vscode-text-field type="text" id="basedn" placeholder="e.g. dc=example,dc=org">Base DN</vscode-text-field>
+			</section>
+
+			<!-- TODO add spacing between form elements -->
+			<!-- TODO complain if the connection name submitted already exists (must be unique) -->
+			<!-- TODO some form elements should be mandatory -->
+
+			<vscode-button onClick="submitForm('save')">Save</vscode-button>
+			<vscode-button onClick="submitForm('test')" appearance="secondary">Test</vscode-button>
+			<script>
+				const vscode = acquireVsCodeApi();
+				function submitForm(command) {
+					vscode.postMessage({
+						command: command,
+						name: document.getElementById("name").value,
+						protocol: document.getElementById("protocol").value,
+						host: document.getElementById("host").value,
+						port: document.getElementById("port").value,
+						binddn: document.getElementById("binddn").value,
+						bindpwd: document.getElementById("bindpwd").value,
+						basedn: document.getElementById("basedn").value
+					});
+				}
+			</script>
+		</body>
+	</html>`;
+
+    // Handle messages from webview to the extension.
+    // See https://code.visualstudio.com/api/extension-guides/webview#passing-messages-from-a-webview-to-an-extension
+    panel.webview.onDidReceiveMessage(
+        message => {
+            // Build connection object.
+            const connection = new LdapConnection(
+                message.name,
+                message.protocol,
+                message.host,
+                message.port,
+                message.binddn,
+                message.bindpwd,
+                message.basedn
+            );
+            switch (message.command) {
+                case 'save':
+                    // Save connection to settings.
+                    LdapConnectionManager.addConnection(connection);
+
+                    // Refresh view so the new connection shows up.
+                    // @todo focus on tree view (in case it was not open) ?
+                    vscode.commands.executeCommand("ldap-explorer.refresh-view");
+
+                    // @todo UX: message "connection was added to your settings" (along with JSON object and location of the settings file i.e. whether it was stored in global or workspace settings)
+                    return;
+
+                case 'test':
+                    // Test connection.
+                    connection.search({}).then(
+                        value => {
+                            vscode.window.showInformationMessage('Connection succeeded');
+                        },
+                        reason => {
+                            vscode.window.showErrorMessage('Connection failed: ' + reason);
+                        }
+                    );
+            }
+        },
+        undefined,
+        context.subscriptions
+    );
+
 }
