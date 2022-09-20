d9550d155fdd3b07b50ab6383e33297dfdf1b892
fengtan <Fengtan@847318.no-reply.drupal.org>
2022-09-19 21:23:44 -0400

    Refactor.

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,8 @@ import * as vscode from 'vscode';
 import { LdapConnectionManager } from './ldapConnectionManager';
 import { LdapDataProvider } from './ldapDataProvider';
 import { LdapTreeItem } from './ldapTreeItem';
-import { createAttributesWebview, createAddConnectionWebview } from './webviews';
+import { createAttributesWebview} from './webviews/createAttributesWebview';
+import { createAddConnectionWebview } from './webviews/createAddConnectionWebview';
 
 // This method is called when the extension is activated (see activationEvents in package.json).
 export function activate(context: vscode.ExtensionContext) {
